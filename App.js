a0a8b7ad5993b3622fa75e5d8df9318c802982e7
NRGK65 <115964527+NRGK65@users.noreply.github.com>
2023-08-23 21:02:21 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Gracias por participar !!!</Text>
+      <Text>Estamos a su disposiciónw</Text>
       <StatusBar style="auto" />
     </View>
   );
